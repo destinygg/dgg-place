cb5cff8446fb10f6be08e57b8a957e53dbd2792e
chatter_here <>
2023-07-22 13:47:30 -0400

    Use 1x pixel scale template

--- dgg-place.user.js
+++ dgg-place.user.js
@@ -18,16 +18,18 @@ if (window.top !== window.self) {
         // const maskPattern = document.createElement('img');
         // maskPattern.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAQAAAD8IX00AAAADklEQVQIW2NggIP/cBYACgsBAIGogeEAAAAASUVORK5CYII=');
 
-        const image = document.createElement("img");
-        image.style = 'display: none';
-        image.onload = () => {
-            image.style = `display: block;position: absolute; left: 0; top: 0; width: ${image.width / 3}px; height: ${image.height / 3}px; image-rendering: pixelated; z-index: 1`;
+
+        const loader = document.createElement('img');
+        const overlay = document.createElement('img');
+        overlay.style = 'display: none';
+        overlay.onload = () => {
+            overlay.style = `display: block;position: absolute; left: 0; top: 0; width: ${overlay.width / 3}px; height: ${overlay.height / 3}px; image-rendering: pixelated; z-index: 1`;
         };
 
         // Add the image as overlay
         const camera = document.querySelector('garlic-bread-embed').shadowRoot.querySelector('garlic-bread-camera');
         const canvas = camera.querySelector('garlic-bread-canvas');
-        canvas.shadowRoot.querySelector('.container').appendChild(image);
+        canvas.shadowRoot.querySelector('.container').appendChild(overlay);
 
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
@@ -59,7 +61,8 @@ if (window.top !== window.self) {
             GM_xmlhttpRequest({
                 method: 'GET',
                 responseType: 'blob',
-                url: `https://rplacecdn.destiny.gg/dgg-place-template-1.png?i=${Date.now()}`,
+                // url: `http://localhost:8000/dgg-place-template-1x.png`,
+                url: `https://rplacecdn.destiny.gg/dgg-place-template-1x.png?i=${Date.now()}`,
                 onload: function (response) {
                     let blob;
                     if (response.response instanceof Blob) {
@@ -71,20 +74,52 @@ if (window.top !== window.self) {
                     }
                     const fr = new FileReader();
                     fr.onload = (e) => {
-                        // reset image.width and image.height; hide to prevent weird flash of content
-                        image.style = 'display: none;width:100%;height:100%;';
-                        image.src = e.target.result;
+                        loader.src = e.target.result;
                     }
                     fr.readAsDataURL(blob);
                 },
             });
         }
 
+        loader.onload = () => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = loader.width * 3;
+            canvas.height = loader.height * 3;
+            ctx.imageSmoothingEnabled = false;
+            ctx.drawImage(loader, 0, 0, loader.width * 3, loader.height * 3);
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            const height = imageData.height;
+            const width = imageData.width;
+            for (let row = 0; row < height; row++) {
+                let rowPixel = row * (width * 4);
+                if ((row + 2) % 3 !== 0) {
+                    for (let col = 0; col < width; col++) {
+                        data[rowPixel + 4 * col + 3] = 0;
+                    }
+                }
+            }
+            for (let row = 0; row < height; row++) {
+                let rowPixel = row * (width * 4);
+                for (let col = 0; col < width; col++) {
+                    if ((col + 2) % 3 !== 0) {
+                        data[rowPixel + 4 * col + 3] = 0;
+                    }
+                }
+            }
+            ctx.putImageData(imageData, 0, 0);
+
+            // reset image.width and image.height; hide to prevent weird flash of content
+            overlay.style = 'display: none;width:100%;height:100%;';
+            overlay.src = canvas.toDataURL();
+        };
+
         function toggle() {
-            if (image.style.display === 'block') {
-                image.style.display = 'none';
+            if (overlay.style.display === 'block') {
+                overlay.style.display = 'none';
             } else {
-                image.style.display = 'block';
+                overlay.style.display = 'block';
             }
         }
 
@@ -104,6 +139,19 @@ if (window.top !== window.self) {
             {position: 'absolute', bottom: '10px', left:'4%', 'z-index': 3});
         addButton('Toggle Template', toggle,
             {position: 'absolute', bottom: '50px', left:'4%', 'z-index': 3});
+
+        function saveCanvas() {
+            const image = canvas.shadowRoot.querySelector('.container > canvas')
+                .toDataUrl('image/png');
+            const link = document.createElement('a');
+            const filename = 'canvas.png';
+            link.setAttribute('href', image);
+            link.setAttribute('download', filename);
+            link.click();
+        }
+
+        window.saveCanvas = saveCanvas;
+        // console.log(saveCanvas.toString())
 
     }, false);
 }
